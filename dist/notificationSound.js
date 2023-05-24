50a97e1fd317263f16cd81b1c5890c7cd0d808a0
FelixFromDiscord <sidorovf996@gmail.com>
2023-05-24 15:20:53 +0300

    add first plugin boilerplate

---
+++ dist/notificationSound.js
@@ -0,0 +1,18 @@
+export const plugin = {
+    data: {
+        author: "FelixFromDiscord",
+        authorId: 634840669898145802n,
+        name: "Notification Sound",
+        description: "Plays a notification sound when someone sends a message in chat or an event happens",
+        repository: "https://github.com/iriscent-chat/plugins",
+    },
+    load: () => {
+        
+    },
+    onmessage: () => {
+
+    },
+    onevent: () => {
+
+    },
+}
