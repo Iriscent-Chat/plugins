97b342daa1bc37301026a4402f8efff621a5ed03
FelixFromDiscord <sidorovf996@gmail.com>
2023-05-26 13:51:04 +0300

    update plugins + add data

--- dist/customThemeLoader.js
+++ dist/customThemeLoader.js
@@ -2,10 +2,9 @@ export default {
     data: {
         author: "FelixFromDiscord",
         authorId: 634840669898145802n,
-        name: "Notification Sound",
-        description: "Plays a notification sound when an event happens",
+        name: "Custom Theme Loader",
+        description: "Loads any custom theme!",
         repository: "https://github.com/iriscent-chat/plugins",
-        url: "https://raw.githubusercontent.com/Iriscent-Chat/plugins/main/dist/notificationSound.js"
     },
     load: () => {
         console.log(this.data.name + " loaded!");
